1d61b5d9fa55445c9270580cb7ae6fefc21e47b7
justinabosconz <justinabosconz@gmail.com>
2026-02-14 18:41:04 +1300

    module7

--- IOD/Module7/second-react app/Components/AppBar.jsx
+++ IOD/Module7/second-react app/Components/AppBar.jsx
@@ -1,113 +1,50 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import AppBar from "@mui/material/AppBar"; // Example with Material-UI
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
-const drawerWidth = 240;
-const navItems = ["Home", "Login", "Bitcoin Rates"];
-
-function DrawerAppBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
+const AppBarComponent = () => {
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            Module 7
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: "#fff" }}>
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-      <Box component="main" sx={{ p: 3 }}></Box>
-    </Box>
-  );
-}
+    <AppBar position="static">
+      <Toolbar>
+        {/* Site Title - Can also be a NavLink to the home page */}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My App Bar
+        </Typography>
 
-DrawerAppBar.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
+        {/* Navigation Links using NavLink */}
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="/"
+            style={({ isActive }) => ({ color: isActive ? "red" : "white" })}
+          >
+            Home
+          </Button>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="/LoginForm"
+            style={({ isActive }) => ({ color: isActive ? "red" : "white" })}
+          >
+            Login
+          </Button>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="/BitcoinRateExercises"
+            style={({ isActive }) => ({ color: isActive ? "red" : "white" })}
+          >
+            BitcoinRates
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
-export default DrawerAppBar;
+export default AppBarComponent;
--- IOD/Module7/second-react app/src/App.jsx
+++ IOD/Module7/second-react app/src/App.jsx
@@ -3,14 +3,14 @@ import EmojiProvider from "../Components/EmojiProvider";
 import AppRoutes from "../Components/AppRoutes";
 import NavBar from "../Components/NavBar";
 import UserProvider from "../Components/UserProvider";
-import DrawerAppBar from "../Components/AppBar";
+import AppBarComponent from "../Components/AppBar";
 
 function App() {
   return (
     <>
       <UserProvider>
         <EmojiProvider>
-          <DrawerAppBar></DrawerAppBar>
+          <AppBarComponent></AppBarComponent>
           <NavBar></NavBar>
           <AppRoutes></AppRoutes>
         </EmojiProvider>
