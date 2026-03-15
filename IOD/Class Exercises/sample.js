db50b28bccdd0e35152046eb646e5d7d9ce61973
justinabosconz <justinabosconz@gmail.com>
2026-03-15 13:44:04 +1300

    semi

--- IOD/Class Exercises/sample.js
+++
@@ -1,4 +0,0 @@
-const epoch = new Date(0); // 0 milliseconds since Jan 1 1970
-const jan2_1970 = new Date(1000 * 60 * 60 * 48); // a full day in milliseconds after Jan 1
-console.log(epoch); // 1970-01-01T00:00:00.000Z
-console.log(+jan2_1970); // 1970-01-02T00:00:00.000Z
